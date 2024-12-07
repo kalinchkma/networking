7067d38ce00152cde71837546292e4d9c4a3e7ed
kalin chakma <chakma.kalin10211@gmail.com>
2024-12-08 01:45:35 +0600

    setup besic routing and file serving

--- http_server/main.go
+++ http_server/main.go
@@ -1,13 +1,56 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) handerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func main() {
-	handler := http.ServeMux{}
+	const port = "8080"
+	const filepathRoot = "./public"
+
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handerRediness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handerReset)
 
 	server := http.Server{
-		Addr:    ":8080",
-		Handler: &handler,
+		Addr:    ":" + port,
+		Handler: mux,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+
+	log.Fatal(server.ListenAndServe())
 }
