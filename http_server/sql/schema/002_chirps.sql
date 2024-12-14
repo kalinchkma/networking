d8b8246262b12bea67711942cbe025903d1fed97
kalin chakma <chakma.kalin10211@gmail.com>
2024-12-14 17:06:52 +0600

    added new model for chrips

---
+++ http_server/sql/schema/002_chirps.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE chrips (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    body Text,
+    user_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- +goose Down
+DROP TABLE chrips;
