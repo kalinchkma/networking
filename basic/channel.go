a9e375d81049d98b39e9bc369104bd245a89a9a4
kalin chakma <chakma.kalin10211@gmail.com>
2024-12-21 20:18:10 +0600

    working with channels

--- basic/channel.go
+++ basic/channel.go
@@ -8,34 +8,77 @@ import (
 
 // Simple send message
 func sendEmail(message string, wg *sync.WaitGroup) {
-  go func() {
-    defer wg.Done()
-    time.Sleep(time.Microsecond * 250)
-    fmt.Printf("Email recived: '%s'\n", message)
-  }()
-  fmt.Printf("Email sent: '%s'\n", message)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Microsecond * 250)
+		fmt.Printf("Email recived: '%s'\n", message)
+	}()
+	fmt.Printf("Email sent: '%s'\n", message)
 }
 
-
 func test(message string, wg *sync.WaitGroup) {
-  sendEmail(message, wg)
-  time.Sleep(time.Microsecond * 500)
-  fmt.Println("-------------------------------------------")
+	sendEmail(message, wg)
+	time.Sleep(time.Microsecond * 500)
+	fmt.Println("-------------------------------------------")
 }
 
 func simulateEmailSend() {
-  fmt.Println("===========Email simulation===============")
-  var wg sync.WaitGroup
-  wg.Add(3)
-  test("Email number 1", &wg)
-  test("Email number 2", &wg)
-  test("Email number 3", &wg)
-  
-  wg.Wait()
-  fmt.Println("Done sending email")
+	fmt.Println("===========Email simulation===============")
+	var wg sync.WaitGroup
+	wg.Add(3)
+	test("Email number 1", &wg)
+	test("Email number 2", &wg)
+	test("Email number 3", &wg)
+
+	wg.Wait()
+	fmt.Println("Done sending email")
+}
+
+func channels() {
+	var wg sync.WaitGroup
+
+	ageChan := make(chan int, 6)
+	wg.Add(6)
+	for i := 10; i <= 15; i++ {
+		go func() {
+			defer wg.Done()
+			ageChan <- i
+		}()
+	}
+	wg.Wait()
+	close(ageChan)
+
+	ageList := []int{}
+	for age := range ageChan {
+		ageList = append(ageList, age)
+	}
+	fmt.Println("age list", ageList)
+}
+
+// fibonacci
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
+}
+
+func concurrentFib(n int) {
+	fib := make(chan int, n)
+
+	go fibonacci(n, fib)
+
+	result := make([]int, 0, n)
+	for f := range fib {
+		result = append(result, f)
+	}
+	fmt.Println(result)
 }
 
 func channelRunner() {
-  simulateEmailSend()
-
+	simulateEmailSend()
+	channels()
+	concurrentFib(10)
 }
--- basic/main.go
+++ basic/main.go
@@ -3,70 +3,69 @@ package main
 import "fmt"
 
 type car struct {
-  brand string
-  model string
-  frontWheel wheel
-  backWheel wheel
+	brand      string
+	model      string
+	frontWheel wheel
+	backWheel  wheel
 }
 
 type wheel struct {
-  radius int
-  material string
+	radius   int
+	material string
 }
 
 type product struct {
-  name string
-  description string
-  brand struct {
-    name string
-    info string
-  }
+	name        string
+	description string
+	brand       struct {
+		name string
+		info string
+	}
 }
 
-func main() { 
-  lambo := car{
-    brand: "Lamborginig",
-    model: "v1",
-    frontWheel: wheel{
-      radius: 120,
-      material: "iron",
-    },
-  }    
+func main() {
+	lambo := car{
+		brand: "Lamborginig",
+		model: "v1",
+		frontWheel: wheel{
+			radius:   120,
+			material: "iron",
+		},
+		backWheel: wheel{},
+	}
 
-  unknown := car{}
+	unknown := car{}
 
-  watch := product{
-    name: "Watch",
-    description: "This is the watch",
-    brand: struct {
-      name string;
-      info string;
-    } {
-      name: "Jumanji",
-      info: "This is the premium watch company",
-    },
-  }
+	watch := product{
+		name:        "Watch",
+		description: "This is the watch",
+		brand: struct {
+			name string
+			info string
+		}{
+			name: "Jumanji",
+			info: "This is the premium watch company",
+		},
+	}
 
-  fmt.Printf("My watch %#v\n", watch)
-  fmt.Printf("My car %#v\n", lambo)
-  fmt.Printf("Not define car %#v\n", unknown)
-  
-  printVariableType(10)
-  printVariableType("ad")
-  printVariableType(23.12)
+	fmt.Printf("My watch %#v\n", watch)
+	fmt.Printf("My car %#v\n", lambo)
+	fmt.Printf("Not define car %#v\n", unknown)
 
-  channelRunner()
+	printVariableType(10)
+	printVariableType("ad")
+	printVariableType(23.12)
+
+	channelRunner()
 }
-
 
 func printVariableType(v interface{}) {
-  switch t := v.(type) {
-  case int:
-    fmt.Printf("%T\n", t)
-  case string:
-    fmt.Printf("%T\n", t)
-  default:
-    fmt.Printf("We don't have this %T type\n", t)
-  }
+	switch t := v.(type) {
+	case int:
+		fmt.Printf("%T\n", t)
+	case string:
+		fmt.Printf("%T\n", t)
+	default:
+		fmt.Printf("We don't have this %T type\n", t)
+	}
 }
-
